854d83bf7f1ffdc1e3dd00c8120cef2fefefdb27
indiakka <78439130+indiakka@user.noreply.github.com>
2024-09-08 13:05:45 +0200

    config utils to connect with the bbdd

---
+++ src/main/java/com/adoptaamor/adoptaamor/utils/DatabaseConnection.java
@@ -0,0 +1,20 @@
+package com.adoptaamor.adoptaamor.utils;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import io.github.cdimascio.dotenv.Dotenv;
+
+public class DatabaseConnection {
+
+    private static final Dotenv dotenv = Dotenv.configure().directory(".gitignore").load();
+
+    private static final String JDBC_URL = "jdbc:postgresql://localhost:5432/adopta-amor";
+    private static final String USERNAME = dotenv.get("USER");
+    private static final String PASSWORD = dotenv.get("PASSWORD");
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(JDBC_URL, USERNAME, PASSWORD);
+    }
+}
