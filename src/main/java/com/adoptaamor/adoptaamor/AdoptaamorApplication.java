537a4cb265bbf9ac77aef9d169788a8945cfaedc
indiakka <78439130+indiakka@user.noreply.github.com>
2024-09-08 13:08:34 +0200

    create application and applicationTest

---
+++ src/main/java/com/adoptaamor/adoptaamor/AdoptaamorApplication.java
@@ -0,0 +1,13 @@
+package com.adoptaamor.adoptaamor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdoptaamorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdoptaamorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/adoptaamor/adoptaamor/AdoptaamorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adoptaamor.adoptaamor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdoptaamorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
