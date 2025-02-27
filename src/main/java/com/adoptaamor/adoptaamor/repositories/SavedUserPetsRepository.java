e9907979c862f5d06d1d5c5d5e11a9ab3b37b775
indiakka <78439130+indiakka@user.noreply.github.com>
2025-02-27 17:39:43 +0100

    changed Integer for long

--- src/main/java/com/adoptaamor/adoptaamor/repositories/SavedUserPetsRepository.java
+++ src/main/java/com/adoptaamor/adoptaamor/repositories/SavedUserPetsRepository.java
@@ -4,6 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.adoptaamor.adoptaamor.models.SavedUserPets;
 
-public interface  SavedUserPetsRepository extends JpaRepository <SavedUserPets, Integer> {
+public interface  SavedUserPetsRepository extends JpaRepository <SavedUserPets, Long> {
     
 }
